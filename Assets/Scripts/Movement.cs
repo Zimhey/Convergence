acb955bd613730d01470cf0bf9052c8817c7cb64
theoncomingfish <mjszpunar@wpi.edu>
2017-11-18 16:46:35 -0500

    kinda started movement?

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction { UP, DOWN, LEFT, RIGHT}
+
+
+public class Movement : MonoBehaviour {
+
+    public Direction direction = Direction.DOWN;
+    private int speed;
+    private Vector3 pos;
+}
