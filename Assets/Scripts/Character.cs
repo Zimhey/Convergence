6f5d524d7368c4545937ca8da4ce60b14934c17f
theoncomingfish <mjszpunar@wpi.edu>
2017-11-18 18:33:50 -0500

    Single move boi can move singularly
    and with friends :D

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour{
+
+    public enum Direction { UP, DOWN, LEFT, RIGHT};
+    public Vector3 pos;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Move(Direction.UP);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Move(Direction.DOWN);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(Direction.LEFT);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(Direction.RIGHT);
+        }
+
+    }
+
+    abstract public void Move(Direction dir);
+}
---
+++ Assets/Scripts/SingleMove.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleMove : Character {
+
+    private void Start()
+    {
+        pos = gameObject.GetComponent<RectTransform>().localPosition;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Move(Direction.UP);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Move(Direction.DOWN);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(Direction.LEFT);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(Direction.RIGHT);
+        }
+
+    }
+    public override void Move(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.UP:
+                {
+                    pos.y += 51.2f;
+                    gameObject.GetComponent<RectTransform>().localPosition = pos;
+                    break;
+                }
+            case Direction.DOWN:
+                {
+                    pos.y -= 51.2f;
+                    gameObject.GetComponent<RectTransform>().localPosition = pos;
+                    break;
+                }
+            case Direction.LEFT:
+                {
+                    pos.x -= 51.2f;
+                    gameObject.GetComponent<RectTransform>().localPosition = pos;
+                    break;
+                }
+            case Direction.RIGHT:
+                {
+                    pos.x += 51.2f;
+                    gameObject.GetComponent<RectTransform>().localPosition = pos;
+                    break;
+                }
+
+        }
+    }
+}
