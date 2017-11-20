12dd3226b90e88dd5a0f73366a86d8a640d0c082
Corey Dixon <zimhey1@gmail.com>
2017-11-19 21:20:24 -0500

    GameManager & UIManager & UI Prefab

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    MainMenu,
+    Loading,
+    WaitForInput,
+    WaitForMoveComplete,
+    Pause
+}
+
+public class GameManager : MonoBehaviour
+{
+    public BoardManager Board;
+    public GameState state;
+    public GameState State
+    {
+        get
+        {
+            return state;
+        }
+        set
+        {
+            if (state != value)
+            {
+                if (value == GameState.MainMenu)
+                    ShowMainMenu();
+                if (value == GameState.Pause)
+                    OnGamePause();
+
+            }
+            state = value;
+        }
+    }
+
+    //public List<SingleMovePlayer> characters;
+
+    // Use this for initialization
+    void Start()
+    {
+        State = GameState.MainMenu;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (State)
+        {
+            case GameState.MainMenu:
+                break;
+            case GameState.Loading:
+                break;
+            case GameState.WaitForInput:
+                break;
+            case GameState.WaitForMoveComplete:
+                break;
+            case GameState.Pause:
+                break;
+        }
+    }
+
+    private void OnGamePause()
+    {
+        //https://answers.unity.com/questions/904429/pause-and-resume-coroutine-1.html
+    }
+
+    private void ShowMainMenu()
+    {
+
+    }
+
+    public List<string> GetLevelList()
+    {
+        List<string> list = new List<string>();
+        list.Add("Test");
+        list.Add("Ohhhh weow");
+        return list;
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UserInterfaceScreens
+{
+    MainMenu,
+    LevelSelect,
+    Pause,
+    None
+}
+
+public class UIManager : MonoBehaviour
+{
+    private UserInterfaceScreens screen;
+    public UserInterfaceScreens Screen
+    {
+        get
+        {
+            return screen;
+        }
+        set
+        {
+            setPanel(screen, false);
+            setPanel(value, true);
+            screen = value;
+        }
+    }
+
+    private void setPanel(UserInterfaceScreens target, bool active)
+    {
+        if (target == UserInterfaceScreens.MainMenu)
+            MainMenuPanel.SetActive(active);
+        else if (target == UserInterfaceScreens.LevelSelect)
+            LevelSelectPanel.SetActive(active);
+        else if (target == UserInterfaceScreens.Pause)
+            PausePanel.SetActive(active);
+    }
+
+    public GameObject MainMenuPanel;
+    public GameObject LevelSelectPanel;
+    public GameObject LevelDropDown;
+    public GameObject PausePanel;
+    public GameManager GM;
+
+    private int levelIndex;
+
+    // Use this for initialization
+    void Start () {
+         // Add Levels
+        UnityEngine.UI.Dropdown dropDownBox = LevelDropDown.GetComponent<UnityEngine.UI.Dropdown>();
+        dropDownBox.ClearOptions();
+        dropDownBox.AddOptions(GM.GetLevelList());
+        dropDownBox.RefreshShownValue();
+        levelIndex = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowMainMenu()
+    {
+        Screen = UserInterfaceScreens.MainMenu;
+    }
+
+    public void ShowLevelSelect()
+    {
+        Screen = UserInterfaceScreens.LevelSelect;
+    }
+
+    public void setLevel(int index)
+    {
+        levelIndex = index;
+    }
+    public void PlayClicked()
+    {
+        
+    }
+
+    public void ShowPause()
+    {
+        Screen = UserInterfaceScreens.Pause;
+    }
+
+    public void Resume()
+    {
+        HideAll();
+    }
+
+    public void HideAll()
+    {
+        Screen = UserInterfaceScreens.None;
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
