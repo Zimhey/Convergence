0e050cfb542aba2d59012c1fa53e71decb80f3c2
LEO-PC\lrbun <lrbunyea@wpi.edu>
2017-11-26 19:51:52 -0500

    I hecked up the goal code, here it is fixed, also removed the second icetrigger file. No more compilation errors. :)

--- Assets/Scripts/Triggers/GoalTrigger.cs
+++ Assets/Scripts/Triggers/GoalTrigger.cs
@@ -15,20 +15,28 @@ public class GoalTrigger : MonoBehaviour {
 	bool Char4Entered = false;
 
 	bool entered = false;
+	bool total = false;
+
+	int numChars;
 
 	void OnTriggerEnter2D(Collider2D other) {
 
 		if (!entered) {
 			entered = true;
-			//Populate object array with all types of existing players on the board
-			//@TODO fix this with other characters
-			Characters = GameObject.FindGameObjectsWithTag ("SingleMove");
-			//Characters = GameObject.FindGameObjectsWithTag ("DoubleMove");
-			//Characters = GameObject.FindGameObjectsWithTag ("IceBoi");
-			//Characters = GameObject.FindGameObjectsWithTag ("?????");
+			if (!total) {
+				//Populate object array with all types of existing players on the board
+				//@TODO fix this with other characters
+				Characters = GameObject.FindGameObjectsWithTag ("SingleMove");
+				//Characters = GameObject.FindGameObjectsWithTag ("DoubleMove");
+				//Characters = GameObject.FindGameObjectsWithTag ("IceBoi");
+				//Characters = GameObject.FindGameObjectsWithTag ("?????");
 
-			//Check length of the array to see how many character's we're dealing with
-			int numChars = Characters.Length;
+				//Check length of the array to see how many character's we're dealing with
+				numChars = Characters.Length;
+
+				Debug.Log (numChars);
+				total = true;
+			}
 
 			//The most hideously disgusting if statement you've ever seen
 			if (numChars == 4) {
